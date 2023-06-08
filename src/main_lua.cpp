b082a9e2a6d68c2600709f993bde3a1d63b3de60
Aubin Detrez <aubin.detrez@gmail.com>
2023-06-08 23:37:32 +0200

    Play with Lua - C++ interface

--- src/main_lua.cpp
+++ src/main_lua.cpp
@@ -7,32 +7,93 @@ extern "C" {
 #include <lauxlib.h>
 }
 
-// *** define C++ function ***
-static int MyCppFunction(lua_State* L) // Lua callable functions must be this format
-{
-    const int num = (int)lua_tonumber(L,1); // get first param from stack
-    const char* str = lua_tostring(L,2); // get second param from stack
-    std::cout << "Hello from C++!" << std::endl;
-    std::cout << "num = " << num << ", str = " << str << std::endl;
-    return 0; // how many params we're passing to Lua
+static void stackDump (lua_State *L) {
+    int i;
+    int top = lua_gettop(L);
+    std::cout << "Stack (Lua): ";
+    for (i = 1; i <= top; i++) {  /* repeat for each level */
+        int t = lua_type(L, i);
+        switch (t) {
+
+            case LUA_TSTRING:  /* strings */
+                std::cout << lua_tostring(L, i);
+                break;
+
+            case LUA_TBOOLEAN:  /* booleans */
+                std::cout << lua_toboolean(L, i) ? "true" : "false";
+                break;
+
+            case LUA_TNUMBER:  /* numbers */
+                std::cout << lua_tonumber(L, i);
+                break;
+
+            default:  /* other values */
+                std::cout << lua_typename(L, t);
+                break;
+
+        }
+        std::cout << "\t";
+    }
+    std::cout << std::endl;
 }
 
+static int editorConfigInt(lua_State* L)
+{
+    const char* str = lua_tostring(L,1);
+    if (!lua_isnumber(L, 2)) {
+        std::cerr << "Error: editorConfigInt() expects an integer" << std::endl;
+        return LUA_ERRERR;
+    }
+    const int num = (int)lua_tonumber(L,2);
+    // lua_isnumber() to check the type
+    std::cout << "editorConfigInt(): Register=" << str << " Value:" << num << std::endl;
+    return LUA_OK;
+}
+
+static int editorConfigStr(lua_State* L)
+{
+    const char* str = lua_tostring(L,1);
+    const char* value = lua_tostring(L,2);
+    std::cout << "editorConfigStr(): Register=" << str << " Value:" << value << std::endl;
+    return LUA_OK;
+}
+
+
+// Lua's manual: https://www.lua.org/manual/5.3/manual.html
 int main(int argc, const char * argv[])
 {
     int rc;
     const char* lua_file = "src/test.lua";
     lua_State* L = luaL_newstate(); // create a new lua instance
     luaL_openlibs(L); // give lua access to basic libraries
-    lua_register(L, "CallMyCppFunction", MyCppFunction); // register our C++ function with Lua
+
+    lua_pushinteger(L, 1);
+    lua_setglobal(L, "editorVersion");
+
+    lua_register(L, "editorConfigStr", editorConfigStr);
+    lua_register(L, "editorConfigInt", editorConfigInt);
+
     rc = luaL_dofile(L, lua_file); // loads the Lua script
     if (rc != 0) {
         std::cerr << "Error: Cannot open lua file: " << lua_file << std::endl;
     }
 
-    // *** call Lua function from C++ ***
-    lua_getglobal(L, "MyLuaFunction"); // find the Lua function
-    lua_pushnumber(L, 73); // push number as first param
-    lua_pushstring(L, "From C++ to Lua"); // push string as second param
-    lua_pcall(L, 2, 0, 0); // call the function passing 2 params
+    lua_getglobal(L, "MyLuaFunction");
+    lua_pushnumber(L, 73); // First argument
+    lua_pushstring(L, "From C++ to Lua"); // Seconds argument
+    lua_pcall(L, /*nargs*/2, /*nresults*/0, /*msgh=return err to stack*/0);
+
+    lua_getglobal(L, "GlobalVariable");
+    if (lua_isnoneornil(L, -1)) {
+        std::cout << "GlobalVariable is nil" << std::endl;
+    }
+
+    // lua_gettop(L) returns the number of elements on the stack
+    // lua_pop(L, 1);
+
+    stackDump(L);
+
+    lua_close(L);
+
     return 0;
 }
--- src/test.lua
+++ src/test.lua
@@ -1,8 +1,12 @@
--- call C++ function from Lua --
-CallMyCppFunction(42,'From Lua to C++')
+print("Editor's LUA API version: "..tostring(editorVersion))
+
+editorConfigInt('FontSize',14)
+editorConfigStr("Font",'/home/phileas/.fonts/JetBrainsMono/ttf/JetBrainsMono-SemiBold.ttf')
 
 -- define Lua function --
 function MyLuaFunction(num, str)
     print("Hello from Lua!")
     print("num = "..tostring(num)..", str = "..str)
 end
+
+GlobalVariable = 42
